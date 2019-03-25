627fdf47ccf42be79f30b253f5e63a8d5dae44ee
YoungWave21 <43156481+YoungWave21@users.noreply.github.com>
2019-03-25 17:16:01 -0600

    Add files via upload

--- Flappy bird/global.js
+++ Flappy bird/global.js
@@ -4,7 +4,6 @@ let spriteThree = document.getElementById("image3");
 let bgimage = document.getElementById("BgImage");
 let pipeOnTop = document.getElementById("pipeUp");
 let pipeOnBottom = document.getElementById("pipeDown");
-let relativeY = 0;
 let player = {
     x: 150,
     y: 100,
@@ -22,16 +21,12 @@ let CutsceneOne = 0;
 let CutsceneTwo = 449;
 let CutsceneThree = 449 * 2;
 let CutsceneFour = 449 * 3;
-let something = [CutsceneOne, CutsceneTwo, CutsceneThree, CutsceneFour];
+let background = [CutsceneOne, CutsceneTwo, CutsceneThree, CutsceneFour];
 let barsx = [800, 1200, 1600, 2000];
-let platform1 = {
-    x: 800,
-    y: -300,
-}
-let platform2 = {
-    x: platform1.x + 400,
-    y: -300,
-}
-let relativeX = player.x + 90;
 let topbarsy = [Math.randomInt(-450, -150), Math.randomInt(-450, -150), Math.randomInt(-450, -150), Math.randomInt(-450, -150)];
 let bottombarsy = [topbarsy[0] + 700, topbarsy[1] + 700, topbarsy[2] + 700, topbarsy[3] + 700];
+let relativeY = 0;
+let relativeX = player.x + 90;
+let topRelativex = player.x + 45;
+let topRelativeY = 0;
+let bottomRelativeY = 0;
--- Flappy bird/main.js
+++ Flappy bird/main.js
@@ -1,29 +1,38 @@
 initGraphics(1250, 600);
 
 requestAnimationFrame(main);
-// let platform1
-
 changetheframe();
 
 function main() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
+    topRelativeY = player.y;
+    bottomRelativeY = player.y + 64;
     relativeY = player.y + 32;
     moveplayervrt();
     for (let n = 0; n < 4; n++) {
-        backgrounddrawing(something[n], n);
+        backgrounddrawing(background[n], n);
     }
 
     ctx.drawImage(whichdrawing, player.x, player.y);
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < barsx.length; i++) {
         ctx.drawImage(pipeOnTop, barsx[i], topbarsy[i], 52, 500);
         barsx[i] -= 2;
+        if (barsx[i] + 52 <= -294) {
+            barsx[i] = cnv.width;
+        }
         ctx.drawImage(pipeOnBottom, barsx[i], bottombarsy[i], 52, 500);
-    }
-
-    if (relativeX >= platform2.x && relativeX <= platform2.x + 52) {
-        if (relativeY >= 0 && relativeY <= platform2.y + 600) {
-            //this is what should happen when the playerenters between the two pipes
+        if (relativeY <= topbarsy[i] + 500 || relativeY >= bottombarsy[i]) {
+            if (relativeX >= barsx[i] && relativeX <= barsx[i] + 52) {
+                // console.log("touched");
+                alert("touched");
+            }
+        }
+        if (topRelativeY <= topbarsy[i] + 500 || bottomRelativeY >= bottombarsy[i]) {
+            if (topRelativex >= barsx[i] && topRelativex <= barsx[i] + 52) {
+                // console.log("touched");
+                alert("touched");
+            }
         }
     }
 
@@ -34,13 +43,12 @@ function main() {
     //loop
     requestAnimationFrame(main);
 }
-let change1 = 0;
 
 function backgrounddrawing(change, index) {
     change--;
-    something[index] = change;
+    background[index] = change;
     if (change + 449 <= 0) {
-        something[index] = cnv.width
+        background[index] = cnv.width
     };
     ctx.drawImage(bgimage, change, 0);
 }
